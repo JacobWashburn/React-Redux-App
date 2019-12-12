24d366e42ec6280f80dbade3121135e3bbadc235
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-12 13:52:31 -0700

    reducer.js: initial setup of state and reducer >>>> reducers/index.js: import/export of reducer

---
+++ redux-api-async/src/Reducers/reducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    greeting: 'Welcome. This is the initial state.'
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
